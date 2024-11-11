95ffc5b833aa0c742c734d7453dc741d9b23b86f
AlexsandroVS <alexsandro.valeriano1@gmail.com>
2024-11-11 16:49:32 -0500

    no se

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
-##!/usr/bin/env bash
+#!/usr/bin/env bash
 # Exit on error
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-python pharmavida/manage.py collectstatic
-python pharmavida/manage.py migrate
+# Convert static asset files
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py migrate
